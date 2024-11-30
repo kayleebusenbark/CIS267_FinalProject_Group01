78ba57c01452c55bf898dc8e2d766da625c0808e
kayleetb <kaybusenbark21@gmail.com>
2024-11-30 00:32:28 -0500

    Revert "Reapply "More Level3 enemy work""
    This reverts commit 9811171bee3a642f7eae858b64bd599bb8d6d35f.

--- Assets/Scripts/Level03Scripts/Level03EnemyHealth.cs
+++ Assets/Scripts/Level03Scripts/Level03EnemyHealth.cs
@@ -4,57 +4,21 @@ using UnityEngine;
 
 public class Level03EnemyHealth : MonoBehaviour
 {
-    public float maxHealth = 3;
-    private float currentHealth;
-    public float destroyDelay = 0.5f;
-    public float pushbackForce = 1f;
-
-
-    private Animator animator;
-
-    private Rigidbody2D rb;
-    private PlayerController player;
-
-    private void Start()
-    {
-        animator = GetComponent<Animator>();
-        rb= GetComponent<Rigidbody2D>();
-        player = FindObjectOfType<PlayerController>();
-
-        currentHealth = maxHealth;
-
-    }
+    public float health = 3;
 
     public void takeDamage(float damage)
     {
-        currentHealth -= damage;
+        health -= damage;
 
-        animator.SetTrigger("hit");
-
-        pushBackFromPlayer();
-
-        if(currentHealth <= 0 )
+        if(health <= 0 )
         {
             defeated();
         }
     }
 
-    private void pushBackFromPlayer()
-    {
-        Vector2 pushDirection = (Vector2)(transform.position - player.transform.position).normalized;
-
-        rb.AddForce(pushDirection * pushbackForce, ForceMode2D.Impulse);
-    }
-
 
     public void defeated()
     {
-
-        animator.SetTrigger("death");
-
-        GetComponent<Collider2D>().enabled = false;
-        this.enabled = false;
-
-        Destroy(gameObject, destroyDelay);
+        Destroy(gameObject);
     }
 }
