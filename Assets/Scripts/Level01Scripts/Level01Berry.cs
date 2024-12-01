67d132e632f0eec2381c00a7f3bcd125b695fa60
Abakr1 <aminahbakr0@gmail.com>
2024-12-01 12:22:39 -0500

    Level01-BerriesUpdated

--- Assets/Scripts/Level01Scripts/Level01Berry.cs
+++ Assets/Scripts/Level01Scripts/Level01Berry.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Level01Berry : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
+
+
 }
