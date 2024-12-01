72e77648257614a481ad3dc29747cadaac3474cf
Abakr1 <aminahbakr0@gmail.com>
2024-12-01 11:52:00 -0500

    Level01-EnemiesUpdated,Berry Collectible

--- Assets/Scripts/Level01Scripts/Level01Enemy.cs
+++ Assets/Scripts/Level01Scripts/Level01Enemy.cs
@@ -6,24 +6,71 @@ using UnityEngine;
 public class Level01Enemy : MonoBehaviour
 {
     private GameObject player;
-    private GameObject gameManager; 
-    private Vector2 playerLocation;
-    private float speed; 
+    private GameObject gameManager;
+    private Vector2 moveLocation;
+    private float timer;
+    [SerializeField]
+    private float speed = 2f;
+    [SerializeField]
+    private float minRange; 
+    [SerializeField]
+    private float maxRange; 
+    public Transform target; 
+    public Rigidbody2D rb;
+
+    public Transform homePos; 
+    private Animator myAnimator; 
+   
     
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player"); 
-
-        
+        //player = GameObject.FindGameObjectWithTag("Player"); 
+        myAnimator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").transform; 
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerLocation = player.transform.position;
-        transform.position = Vector2.MoveTowards(transform.position, playerLocation, speed * Time.deltaTime);
+       // playerLocation = player.transform.position;
+       // transform.position = Vector2.MoveTowards(transform.position, playerLocation, speed * Time.deltaTime);
+
+        if(Vector3.Distance(target.position, transform.position) <= maxRange && Vector3.Distance(target.position, transform.position) >= minRange)
+        {
+            followPlayer();
+        }
+        else
+        {
+            goHome();
+        }
+        
+
+        
+    }
+
+
+    public void followPlayer()
+    {
+        myAnimator.SetBool("isMoving", true);
+        myAnimator.SetFloat("moveX", target.position.x - transform.position.x);
+        myAnimator.SetFloat("moveY", target.position.y - transform.position.y);
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime); 
+
+
+    }
+
+    public void goHome()
+    {
+        myAnimator.SetFloat("moveX", homePos.position.x - transform.position.x);
+        myAnimator.SetFloat("moveY", homePos.position.y - transform.position.y);
+        transform.position = Vector3.MoveTowards(transform.position, homePos.position, speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, homePos.position) == 0 )
+        {
+            myAnimator.SetBool("isMoving", false); 
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -34,7 +81,7 @@ public class Level01Enemy : MonoBehaviour
         {
 
         }
-        else
+        else if(Vector3.Distance(target.position, transform.position) >= maxRange)
         {
             isDead(); 
         }
--- Assets/Scripts/Level01Scripts/Level01Spawner.cs
+++ Assets/Scripts/Level01Scripts/Level01Spawner.cs
@@ -1,40 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Level01Spawner : MonoBehaviour
 {
     public GameObject spawnedEnemies;
+    public float spawnTime;
+    public float coolDown = 1f; 
 
     //to control the time/ speed of enemies spawning 
-    public float time; 
-    public float timeDelay;
+    //public float time; 
+    //public float timeDelay;
 
     //the gameObjects are will be where the enemeies spawn 
-    public GameObject[] spawnLocations; 
+    public GameObject[] spawnLocations;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnTime = coolDown;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
+        if(spawnTime > 0)
+        {
+            spawnTime -= Time.deltaTime;
+        }
+
+       // time += Time.deltaTime;
 
 
-        if (time >= timeDelay)
+        if (spawnTime <= 0)
         {
             spawnEnemy();
-            time = 0f; 
+            spawnTime = coolDown; 
         }
     }
 
     private void spawnEnemy()
     {
         int spawnNum = Random.Range(0, spawnLocations.Length);
-
+        
         GameObject spawnLocation = spawnLocations[spawnNum];
 
         Instantiate(spawnedEnemies); 
