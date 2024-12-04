a8349ae98bdef4b07d0d8c8e930d85a545ab54b5
JosephStanford <floopymuffins@gmail.com>
2024-12-04 17:13:41 -0500

    12.4.24
    Worked on cyclops logic.

--- Assets/Scripts/Level02Scripts/CyclopsAI.cs
+++ Assets/Scripts/Level02Scripts/CyclopsAI.cs
@@ -7,12 +7,22 @@ public class CyclopsAI : MonoBehaviour
 {
     public float cyclopsSpeed = 1f;
     public float stopDistance = .4f;
+    public float cyclopsFollowingDistance = 1f;
+
+    public Level02EnemyHealth cyclopsHealth;
+
+    public GameObject laserPrefab;
 
     private bool isFiringLaser = false;
-    private bool isThrowingRock = false;
-    private bool isStomping = false;
+    private bool isWalking = false;
+    //private bool isThrowingRock = false;
+    //private bool isStomping = false;
+
+    private PlayerController player;
 
     private Animator animator;
+
+    public int damage;
     
     // Start is called before the first frame update
     void Start()
@@ -31,23 +41,25 @@ public class CyclopsAI : MonoBehaviour
 
     }
 
-    private void StompAtPlayer()
-    {
+    //private void StompAtPlayer()
+    //{
 
-    }
+    //}
 
     private void ShootLaser()
     {
-
+        if (stopDistance == .4f) 
+        {
+            isFiringLaser = true;
+        }
     }
 
-    private void ThrowRock()
-    {
+    //private void ThrowRock()
+    //{
 
-    }
+    //}
     
     private void CyclopsIsDead()
     {
-
     }
 }
