f74cef9878be15ff45570fbb23ad66fa4310c358
kayleetb <kaybusenbark21@gmail.com>
2024-12-05 22:53:16 -0500

    Cyclops and orc take damage

--- Assets/Scripts/PlayerAttack01.cs
+++ Assets/Scripts/PlayerAttack01.cs
@@ -39,7 +39,7 @@ public class PlayerAttack01 : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("SkeletonEnemy") || collision.CompareTag("WolfEnemy") || collision.CompareTag("Wizard") || collision.CompareTag("BushMonster") || collision.CompareTag("FlowerEnemy"))
+        if(collision.CompareTag("SkeletonEnemy") || collision.CompareTag("WolfEnemy") || collision.CompareTag("Wizard") || collision.CompareTag("BushMonster") || collision.CompareTag("FlowerEnemy") || collision.CompareTag("Cyclops") || collision.CompareTag("Orc"))
         {
             enemyHealth = collision.GetComponent<Level03EnemyHealth>();
 
